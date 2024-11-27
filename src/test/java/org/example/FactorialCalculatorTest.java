c8609018801a6352db32223d44941fc6cd4283d1
StepanovaNika23 <stepanovanika2412@gmail.com>
2024-11-27 16:21:56 +0300

    TestNG

--- src/test/java/org/example/FactorialCalculatorTest.java
+++ src/test/java/org/example/FactorialCalculatorTest.java
@@ -1,36 +1,45 @@
 package org.example;
 
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
-public class FactorialCalculatorTest {
+public class FactorialCalculatorTest{
     private final FactorialCalculator calculator = new FactorialCalculator();
     @Test
-    void testFactorialZero() {
-        Assertions.assertEquals(1, calculator.calculateFactorial(0));
-    }
-    @Test
-    void testFactorialOne() {
-        Assertions.assertEquals(1, calculator.calculateFactorial(1));
+    public void testFactorialZero() {
+        Assert.assertEquals(1, calculator.calculateFactorial(0));
     }
 
     @Test
-    void testFactorialPositive() {
-        Assertions.assertEquals(120, calculator.calculateFactorial(5));
-    }
-
-    @ParameterizedTest
-    @CsvSource({"0,1", "1,1", "5,120", "6,720"})
-    void testFactorialParameterized(int n, long expected) {
-        Assertions.assertEquals(expected, calculator.calculateFactorial(n));
+    public void testFactorialOne() {
+        Assert.assertEquals(1, calculator.calculateFactorial(1));
     }
 
     @Test
-    void testFactorialNegative() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> calculator.calculateFactorial(-1));
+    public void testFactorialPositive() {
+        Assert.assertEquals(120, calculator.calculateFactorial(5));
+    }
+    @DataProvider(name = "factorialData")
+    public Object[][] factorialData() {
+        return new Object[][]{
+                {0, 1},
+                {1, 1},
+                {5, 120},
+                {6, 720},
+                {10, 3628800}
+        };
+        };
+
+    @Test(dataProvider = "factorialData")
+    public void testFactorialParameterized(int n, long expected) {
+        Assert.assertEquals(expected, calculator.calculateFactorial(n));
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testFactorialNegative() {
+        calculator.calculateFactorial(-1);
     }
 
 }
